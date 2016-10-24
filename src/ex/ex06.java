7aa99c667bf71f72c946cbf3c0b12e5fd332c101
蕭佑誠 <105021028@live.asia.edu.tw>
2016-10-24 19:27:55 +0800

    ex

--- src/ex/ex06.java
+++ src/ex/ex06.java
@@ -9,8 +9,29 @@ public class ex06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入共幾筆資料");
+		int n =scn.nextInt();
+		int[]data =new int [n];
+		int max =0;
+		int s= 0,k=0;
+		for(int i =0;i<n;i++){
+			data[i]=scn.nextInt();
+		}
+		for(int a =0;a<n;a++){
+			for( int b =0;b<n;b++){
+				if(data[b]>max){
+					max=data[b];
+					s=b;
+				}
+			}
+		k=max;
+		data[s]=-1;
+		max=0;
+		s=0;
+		System.out.println(k);
 		
-
+		}
 	}
 
 }
